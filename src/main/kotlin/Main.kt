934ffec66ba0f403d25d6bf81da1dd349de3bc9c
Andrey <areyka1@gmail.com>
2024-07-29 19:47:11 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    /*Вывод к*/
+    println("Hello")
+    println("World")
+
+    print("Hello")
+    print("Kotlin")
+    }
